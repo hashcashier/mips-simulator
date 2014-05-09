61f4b7f1a9e4247e0a2329da32a7a6cb54b43cfc
AbdullRahman ElHusseini <abdullrahman.elhusseni@robustastudio.com>
2014-05-09 15:58:49 +0200

    Adding class MUX

---
+++ src/Mux.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+
+public class Mux {
+		String select;
+		ArrayList<String> input;
+		
+	public Mux(String select, ArrayList<String> input)
+		{
+			this.select = select;
+			this.input = input;
+		}
+	
+	public String getOutput()
+		{
+			try
+				{
+					int position = Integer.parseInt(select, 2);
+					if(input.get(position) !=null)
+						return input.get(position);
+					return null;
+				}
+			catch(Exception e)
+			{
+				return null;
+			}
+		}
+	
+	public void setSelect(String newSelect)
+		{
+			select = newSelect;
+		}
+}
