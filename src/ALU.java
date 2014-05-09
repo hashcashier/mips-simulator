724072a22da157a83cfc89130e9a9f7d15163cf0
Khaled Helmy <khaled.a.helmy@gmail.com>
2014-05-09 18:00:16 +0200

    Adding ALU basic structure

--- src/ALU.java
+++ src/ALU.java
@@ -1,4 +1,39 @@
-
 public class ALU {
 	
+	private String inputA, inputB;
+	
+	public ALU(String inputA, String inputB, String operation) {
+		this.inputA = inputA;
+		this.inputB = inputB;
+		this.operation = operation;
+	}
+
+	public String getInputA() {
+		return inputA;
+	}
+
+	public void setInputA(String inputA) {
+		this.inputA = inputA;
+	}
+
+	public String getInputB() {
+		return inputB;
+	}
+
+	public void setInputB(String inputB) {
+		this.inputB = inputB;
+	}
+	
+	public String execute(String operation) {
+		if (operation == "ADD") {
+			
+		}
+		else if (operation == "ADDI") {
+			
+		}
+		else if (operation == "SUB") {
+			
+		}
+		return "";
+	}
 }
