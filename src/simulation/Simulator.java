b40130912b918f79973747e68ca377a0a2b38bf7
Rami Khalil <thesavagekoder@gmail.com>
2014-05-24 00:41:00 +0300

    Simulator

--- src/simulation/Simulator.java
+++ src/simulation/Simulator.java
@@ -18,7 +18,18 @@ public class Simulator {
 	AbstractDatapath datapath;
 
 	public Simulator(String filePath, String dataPath, int programOffset)
-			throws FileNotFoundException, DuplicateLabelException, UnkownInstructionException {
+			throws FileNotFoundException, DuplicateLabelException,
+			UnkownInstructionException {
+		assemble(filePath, dataPath, programOffset);
+	}
+
+	public Simulator() {
+
+	}
+
+	public void assemble(String filePath, String dataPath, int programOffset)
+			throws FileNotFoundException, DuplicateLabelException,
+			UnkownInstructionException {
 		ASMReader assemblyFileReader = new ASMReader(filePath);
 		String[] rawInstructions = assemblyFileReader.getInstructions(), rawData = assemblyFileReader
 				.getData();
@@ -31,47 +42,59 @@ public class Simulator {
 		} catch (UnkownLabelException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}catch (UnkownDatapathException e) {
+		} catch (UnkownDatapathException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
 
-	}
-	
 	public void step() throws InvalidOperationException {
-		datapath.nextStep();
+		if(datapath != null)
+			datapath.nextStep();
 	}
-	
+
 	public void run() throws InvalidOperationException {
-		while(datapath.nextStep());
+		if(datapath != null)
+			while (datapath.nextStep());
 	}
-	
+
 	public Hashtable<String, String> getControlSignals() {
-		//TODO Wait for Azazy..
+		// TODO Wait for Azazy..
 		return new Hashtable<String, String>();
 	}
-	
+
 	public Hashtable<String, String> getRegisterMemoryContents() {
+		if(datapath == null)
+			return new Hashtable<String, String>(); 
 		return datapath.getRegisterContents();
 	}
 
 	public Hashtable<Long, String> getDataMemoryContents() {
+		if(datapath == null)
+			return new Hashtable<Long, String>();
 		return datapath.getDataMemoryContents();
 	}
 
 	public Hashtable<Long, String> getInstructionMemoryContents() {
+		if(datapath == null)
+			return new Hashtable<Long, String>();
 		return datapath.getInstructionMemoryContents();
 	}
-	
+
 	public Hashtable<String, String> getPipelineRegistersContents() {
+		if(datapath == null)
+			return new Hashtable<String, String>();
 		return datapath.getPipelineRegistersContents();
 	}
-	
+
 	public int getProgramCounterValue() {
+		if(datapath == null)
+			return 0;
 		return datapath.getProgramCounterValue();
 	}
-	
+
 	public void setMemoryContent(long address, String value) {
-		datapath.setMemoryContent(address, value);
+		if(datapath != null)
+			datapath.setMemoryContent(address, value);
 	}
 }
