a3a70a2f0dc627391840f6c7f8b319f166ffc87e
Rami Khalil <thesavagekoder@gmail.com>
2014-05-24 08:24:24 +0300

    bug

--- src/peripherals/Adder.java
+++ src/peripherals/Adder.java
@@ -5,9 +5,12 @@ import java.math.BigInteger;
 public class Adder {
 	public static String add(String a, String b) {
 		BigInteger ai = new BigInteger(a, 2), bi = new BigInteger(b, 2);
-		String result = ai.add(bi).toString(2);
+		System.out.println(ai);
+		System.out.println(bi);
+		BigInteger res = ai.add(bi);
+		String result = res.toString(2);
 		while(result.length() < Math.max(a.length(), b.length()))
-			result = result.charAt(0) + result;
+			result = (res.compareTo(BigInteger.ZERO) >= 0 ? "0" : "1") + result;
 		return result.substring(0, Math.min(32, result.length()));
 	}
 }
