4a09fdc4342f1518a500aaa50737201944356cba
Rami Khalil <thesavagekoder@gmail.com>
2014-05-26 00:42:58 +0300

    Jal

--- src/datapath/implementation/pipelined/registers/MEMWB.java
+++ src/datapath/implementation/pipelined/registers/MEMWB.java
@@ -2,18 +2,18 @@ package datapath.implementation.pipelined.registers;
 
 import registers.RegisterManager;
 
-
 public class MEMWB extends AbstractPipelineRegister {
 
 	private static final String[] inputs = { "RegWrite", "MemToReg",
-			"RegisterRd", "Rs", "Rt" };
+			"RegisterRd", "Rs", "Rt", "PC" };
 	private static final String[] outputs = { "RegWrite", "MemToReg",
-			"RegisterRd", "Rs", "Rt" };
-	private static final String[] initial	= {"0", "0", "00000", RegisterManager.zeros32(), RegisterManager.zeros32()}; 
-	
+			"RegisterRd", "Rs", "Rt", "PC" };
+	private static final String[] initial = { "0", "0", "00000",
+			RegisterManager.zeros32(), RegisterManager.zeros32(), RegisterManager.zeros32() };
+
 	public MEMWB() {
 		super(inputs, outputs, "MEM/WB");
-		for(int i = 0; i < initial.length; i++) {
+		for (int i = 0; i < initial.length; i++) {
 			super.inputs[i] = initial[i];
 			super.outputs[i] = initial[i];
 		}
