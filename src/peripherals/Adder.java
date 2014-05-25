a0e88eb7d672ed2d936951dadb63d774c766a3b6
Rami Khalil <thesavagekoder@gmail.com>
2014-05-25 22:38:27 +0300

    modifications.

--- src/peripherals/Adder.java
+++ src/peripherals/Adder.java
@@ -11,7 +11,7 @@ public class Adder {
 		String result = res.toString(2);
 		while(result.length() < Math.max(a.length(), b.length()))
 			result = (res.compareTo(BigInteger.ZERO) >= 0 ? "0" : "1") + result;
-		System.out.println("Branch: " + ai + ' ' + bi + ' ' + result);
+//		System.out.println("Branch: " + ai + ' ' + bi + ' ' + result);
 		return result.substring(0, Math.min(32, result.length()));
 	}
 }
